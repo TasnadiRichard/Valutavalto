fea564dd18ce190232cd1c9078675abe39000080
TasnadiRichard <tasnadiricsi88@gmail.com>
2024-03-05 10:56:50 +0100

    Add project files.

---
+++ Valutavalto/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Valutavalto
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+    }
+}
